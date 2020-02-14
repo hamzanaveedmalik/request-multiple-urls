d63b294bee873b2d11da7cf686dff835fd3d4e94
Hamza Naveed Malik <hamzanaveedmalik@gmail.com>
2020-02-14 16:12:48 +0000

    Pushing Version 1.0.0 to Git

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const fetch = require('node-fetch')
+
+module.exports = requestMultipleUrls = (parsedUrls) => {
+    let data = Promise.all(
+        parsedUrls.map(url => {
+            fetch(url)
+                .then(checkStatus)
+                .then(parseJSON)
+                .catch(err => `The following error has occurred; ${err}`)
+        })
+    )
+    return data;
+}
+
+const checkStatus = (res) => {
+    if (res.ok) { return Promise.resolve(res) }
+    else { return Promise.reject(new Error(res.statusText)) }
+}
+
+const parseJSON = (res) => { return res.json() }
