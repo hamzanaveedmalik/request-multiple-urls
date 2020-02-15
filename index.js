4c8fc8448168f4ddd05c2c919326f7614f0c2620
Hamza Naveed Malik <hamzanaveedmalik@gmail.com>
2020-02-15 16:39:05 +0000

    parseJSON function additions

--- index.js
+++ index.js
@@ -3,24 +3,26 @@
 const fetch = require('node-fetch')
 
 module.exports = function requestMultipleUrls(urls) {
-    let data = Promise.all(
-        urls.map(url => {
+    var data = Promise.all(
+        urls.map(url =>
             fetch(url)
                 .then(checkStatus)
                 .then(parseJSON)
-                .catch(err => `The following error has occurred; ${err}`)
-        })
+                .catch(error => console.log(`The following error has occurred; ${error}`))
+        )
     )
     return data;
 }
 
 const checkStatus = (res) => {
-    if (res.ok) {
-        return Promise.resolve(res)
-    }
-    else {
-        return Promise.reject(new Error(res.statusText))
-    }
+    if (res.ok) { return Promise.resolve(res) }
+    else { return Promise.reject(new Error(res.statusText)) }
 }
 
-const parseJSON = (res) => { return res.json() }
+const parseJSON = (res) => {
+    return {
+        url: res.url,
+        status: res.statusText,
+        body: res.json()
+    }
+}
