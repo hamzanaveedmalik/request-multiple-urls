6f3c6d63dfb116e13bb7b7c927a066802d232438
Hamza Naveed Malik <hamzanaveedmalik@gmail.com>
2020-02-16 00:36:52 +0000

    Codacity Improvement

--- index.js
+++ index.js
@@ -2,18 +2,6 @@
 
 const fetch = require("node-fetch");
 
-module.exports = function requestMultipleUrls(urls) {
-    var data = Promise.all(
-        urls.map(url => (
-            fetch(url)
-                .then(checkStatus)
-                .then(parseJSON)
-                .catch((error) => { console.log(`The following error has occurred: ${error}`); })
-        ))
-    )
-    return data;
-}
-
 const checkStatus = (res) => {
     if (res.ok) { return Promise.resolve(res); }
     else { return Promise.reject(new Error(res.statusText)); }
@@ -26,3 +14,15 @@ const parseJSON = (res) => {
         body: res.json()
     }
 }
+module.exports = function requestMultipleUrls(urls) {
+    var data = Promise.all(
+        urls.map(url => (
+            fetch(url)
+                .then(checkStatus)
+                .then(parseJSON)
+                .catch(error => { console.log(`The following error has occurred: ${error}`) })
+        ))
+    )
+    return data;
+}
+
